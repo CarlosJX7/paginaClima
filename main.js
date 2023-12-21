d6260ea9c45e62d33612a3e3bc66d943b90b1d88
Carlos <carlos.jx7@gmail.com>
2023-12-21 10:49:49 +0100

    e

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
       "count": 1,
       "data": [
         {
-          "app_temp": 9.4,
+          "app_temp": 2,
           "aqi": 31,
           "city_name": "Madrid",
           "clouds": 0,
@@ -36,10 +36,28 @@ document.addEventListener('DOMContentLoaded', function () {
     const weatherInfo = weatherData.data[0];
     const weatherInfoContainer = document.getElementById('weather-info');
 
+    // Utiliza un bloque switch para manejar diferentes casos de temperatura
+    let  imagenPath;
+
+    switch (true) {
+      case weatherInfo.app_temp >= 5 && weatherInfo.app_temp <= 10:
+          imagenPath = 'img/friazo.jpeg';
+          break;
+      case weatherInfo.app_temp > 10:
+          imagenPath = 'img/chill.jpeg';
+          break;
+      case weatherInfo.app_temp < 10:
+          imagenPath = 'img/el_demon.jpeg';
+          break;
+      default:
+          // Si la temperatura no coincide con ninguno de los casos, puedes establecer una imagen predeterminada o dejarla sin definir
+          //imagenPath = 'imagen-default.jpg';
+  }
+
     // Crea un HTML con la información
     const htmlContent = `
     <h1>${weatherInfo.app_temp} °C</h1>
-
+    <img src="${imagenPath}" alt="mood">
       <div class="container">
         <div class="box">  
             <p>Índice de Calidad del Aire: ${weatherInfo.aqi}</p>
